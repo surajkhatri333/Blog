ad5196fd24e23d61e544f988bd3cc1d88dd18c9d
surajkhatri333 <surajkhatr1234567@gmail.com>
2024-11-24 21:08:12 +0530

    Implement authetication using jwt

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,100 +1,215 @@
 import { Link } from 'react-router-dom';
 import styles from '../styles/Header.module.css'
-import {  useState, } from "react";
+import { useEffect, useMemo, useState, } from "react";
 import propTypes from 'prop-types'
-// import axios from 'axios';
+import axios from 'axios';
 
 
-const Header = ( { isLogin ,onLogout ,userEmail} ) => {
+const Header = ({ isLogin, onLogout, userEmail }) => {
     const [tab, settab] = useState(false);
-    console.log(isLogin,onLogout,userEmail)
-
-    // useEffect(() => async ()=>{
-    //     // Check if token exists in cookies
-    //     const token = document.cookie.split('; ').find(row => row.startsWith('token='));
-    //     if (token) {
-    //         // Token exists, make a request to verify it
-    //          await axios.get("http://localhost:8080/check", { withCredentials: true })
-    //             .then((res) => {
-    //                 if (res.data.isLogin) {
-    //                     isLogin(res.data.userEmail); // Assuming handleLogin sets the state
-    //                     // setisLogin(true);
-    //                     // setuserEmail(res.data.userEmail);
-    //                 }
-    //             })
-    //             .catch((err) => {
-    //                 console.error("Error verifying token", err);
-    //                 isLogin(null);
-    //             });
-    //     }
-    // }, [handleLogin]);
-      
+    const [userdata, setuserData] = useState();
+    const [isAdmin, setIsAdmin] = useState(false);
 
     const showTab = () => {
-        settab(prev => {
-            const newTab = !prev;
-            if (newTab === true) {
-                document.querySelector(".tabBar").style.display = 'block';
-            }
-            else {
-                document.querySelector(".tabBar").style.display = 'none';
-            }
-            return newTab;
-        })
-
+        settab(prev => !prev)
     };
+    const userData = async () => {
+        try {
+            const response = await axios.get(`http://localhost:8080/user/${userEmail}`);
+            console.log(response)
+            console.log(response.data.users)
+            setuserData(response.data.users);
+            console.log("users are : ", userdata)
+        }
+        catch (Err) {
+            console.log("Error in get user from backend", Err)
+        }
+
+    }
+    useEffect(() => {
+        if (isLogin) {
+            const checkAdminStatus = async () => {
+                try {
+                    const response = await axios.get('http://localhost:8080/check', { withCredentials: true });
+                    console.log(response);
+                    setIsAdmin(response.data.isAdmin);
+                }
+
+                catch (error) {
+                    console.error('Error verifying admin status:', error);
+                }
+            }
+
+            checkAdminStatus();
+            userData()
+        }
+        else {
+            console.log("User is not logged in, skipping admin check.");
+            setIsAdmin(false);
+        }
+
+
+
+    }, [isLogin]);
+
+
+
     return (
         <>
             <div className={styles.header}>
                 <div className={styles.logo}>LOGO</div>
-                <div className={styles.tabBar}>
+                {/* <div className={styles.tabBar} style={{display : tab ? "block" : "none"}} > */}
+                <div className={`${styles.tabBar} ${tab ? styles.show : ''}`} >
                     <div className={styles.tab} id="tabs1"><Link to="/">HOME</Link></div>
                     <div className={styles.tab} id="tabs2"><Link to="/create">CREATE BLOG</Link></div>
-                    <div className={styles.tab} id="tabs3"><Link to={`/Myblogs/${userEmail}`} >MY BLOGS</Link></div>
-                    <div className={styles.tab} id="tabs4"><Link to ={"/Dashboard"}>Dashboard</Link></div>
-                    <div className={styles.tab} id="tabs5"><Link to ={"/Admin"}>ADMIN</Link></div>
+                    <div className={styles.tab} id="tabs2" style={{ color: "black" }} onClick={() => window.scrollTo({ top: window.innerHeight, behavior: 'smooth' })} >EXPLORE BLOG</div>
+                    {isLogin && (<div className={styles.tab} id="tabs3"><Link to={`/Myblogs/${userEmail}`} >MY BLOGS</Link></div>)}
+                    {isAdmin && (
+                        <>
+                            <div className={styles.tab} id="tabs4"><Link to={"/Dashboard"}>Dashboard</Link></div>
+                            <div className={styles.tab} id="tabs5"><Link to={"/Admin"}>ADMIN</Link></div>
+                        </>
+                    )}
+
                 </div>
                 <div className={styles.account}>
+                    {isLogin ?
+                        (
+                            <>
+                                <div id="profileinfo">
+                                    <img
+                                        src={`http://localhost:8080/${userdata.profileAvatar.replace('\\', '/')}`}
+                                        alt="User Avatar"
+                                        style={{ width: '40px', height: '40px', borderRadius: '50%' }}
+                                    />
+                                    <div id={styles.profile} ></div>
+                                </div>
 
-                    {
-                        isLogin ?
-                            (<button onClick={onLogout}>
-                                Logout
-                            </button>
-                            ) :
-                            (<button >
-                                <Link to={"/login"}>login</Link>
-                            </button>
-                            )
+                                <button onClick={onLogout}>LOGOUT </button>
+                            </>
+                        ) :
+                        (<button>
+                            <Link to={"/login"}>Login</Link>
+                        </button>)
                     }
-                   
-                        <button>
-                            <Link to={"/sign up"}>Sign Up</Link>
-                        </button>
-                    
 
-
-
+                    <button>
+                        <Link to={"/sign up"}>Sign Up</Link>
+                    </button>
                 </div>
-                <i className="fa-duotone fa-solid fa-bars" id={styles.hamIcon} onClick={showTab} value={tab} >AAA</i>
-
+                <i className="fa-solid fa-bars" id={styles.hamIcon} onClick={showTab}>
+                    {/* {tab ? 'Close' : 'Menu'} */}
+                </i>
             </div>
 
-           
+
         </>
-        
+
     )
 
-    
+
 }
 Header.propTypes = {
-    isLogin : propTypes.bool.isRequired,
-    onLogout : propTypes.func.isRequired,
-    userEmail : propTypes.string.isRequired,
-    handleLogin : propTypes.func.isRequired
+    isLogin: propTypes.bool.isRequired,
+    onLogout: propTypes.func.isRequired,
+    userEmail: propTypes.string.isRequired,
+    handleLogin: propTypes.func.isRequired
 }
 
 export default Header
 
 
 
+
+
+
+// import { Link } from 'react-router-dom';
+// import styles from '../styles/Header.module.css';
+// import { useEffect, useState } from 'react';
+// import PropTypes from 'prop-types';
+// import axios from 'axios';
+
+// const Header = ({ isLogin, onLogout, userEmail }) => {
+//     const [tab, setTab] = useState(false);
+//     const [isAdmin, setIsAdmin] = useState(false);
+
+//     const toggleTab = () => setTab((prev) => !prev);
+
+//     useEffect(() => {
+//         if (isLogin) {
+//             const checkAdminStatus = async () => {
+//                 try {
+//                     const response = await axios.get('http://localhost:8080/check', { withCredentials: true });
+//                     console.log(response);
+//                     setIsAdmin(response.data.isAdmin);
+//                 }
+
+//                 catch (error) {
+//                     console.error('Error verifying admin status:', error);
+//                 }
+//             }
+
+//             checkAdminStatus();
+//         }
+//         else {
+//             console.log("User is not logged in, skipping admin check.");
+//             setIsAdmin(false);
+//         }
+//     }, [isLogin]);
+
+//     return (
+//         <div className={styles.header}>
+//             <div className={styles.logo}>LOGO</div>
+
+//             <div className={`${styles.tabBar} ${tab ? styles.show : ''}`}>
+//                 <div className={styles.tab}>
+//                     <Link to="/">HOME</Link>
+//                 </div>
+
+//                 <div className={styles.tab}>
+//                     <Link to="/create">CREATE BLOG</Link>
+//                 </div>
+
+
+//                 <div className={styles.tab}>
+//                     <Link to={`/Myblogs/${userEmail}`}>MY BLOGS</Link>
+//                 </div>
+
+
+//                 {isAdmin && (
+//                     <>
+//                         <div className={styles.tab}>
+//                             <Link to="/Dashboard">DASHBOARD</Link>
+//                         </div>
+//                         <div className={styles.tab}>
+//                             <Link to="/Admin">ADMIN</Link>
+//                         </div>
+//                     </>
+//                 )}
+//             </div>
+
+//             <div className={styles.account}>
+//                 {isLogin ? (
+//                     <button onClick={onLogout}>Logout</button>
+//                 ) : (
+//                     <Link to="/login">
+//                         <button>Login</button>
+//                     </Link>
+//                 )}
+//                 <Link to="/signup">
+//                     <button>Sign Up</button>
+//                 </Link>
+//             </div>
+
+//             <i className={`fa-solid fa-bars ${styles.hamIcon}`} onClick={toggleTab}></i>
+//         </div>
+//     );
+// };
+
+// Header.propTypes = {
+//     isLogin: PropTypes.bool.isRequired,
+//     onLogout: PropTypes.func.isRequired,
+//     userEmail: PropTypes.string.isRequired,
+// };
+
+// export default Header;
