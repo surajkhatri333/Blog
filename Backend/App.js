995c18dd4c4260b0ce4dcd5a6bd823ef186c8d16
surajkhatri333 <surajkhatr1234567@gmail.com>
2024-12-06 23:40:29 +0530

    Add some files changes for deployment

--- Backend/App.js
+++ Backend/App.js
@@ -11,7 +11,7 @@ import { upload } from './public/src/middleware/multer.middleware.js'
 const app = express();
 
 app.use(cors({
-    origin: 'https://blog-frontend-gray-xi.vercel.app/',
+    origin: 'https://blog-frontend-gray-xi.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
@@ -37,6 +37,10 @@ import adminRouter from './public/src/routes/adminRoutes.router.js'
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/admin", adminRouter)
+
+app.get('/test', (req, res) => {
+    res.send('Test route working!');
+  });
 
 app.get("/check",verifyJwt, (req, res) => {
     // This route will only be reached if the token is valid
