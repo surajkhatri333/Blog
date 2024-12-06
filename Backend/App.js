083c9b8ffd608becac291ef083e3178283b5d54c
surajkhatri333 <surajkhatr1234567@gmail.com>
2024-12-06 23:28:25 +0530

    Add some files changes for deployment

--- Backend/App.js
+++ Backend/App.js
@@ -11,7 +11,7 @@ import { upload } from './public/src/middleware/multer.middleware.js'
 const app = express();
 
 app.use(cors({
-    origin: 'https://blog-frontend-gray-xi.vercel.app',
+    origin: 'https://blog-frontend-gray-xi.vercel.app/',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
