8182722a3bcd19cf11915f7d7413459f5374364c
surajkhatri333 <surajkhatr1234567@gmail.com>
2025-07-03 12:55:33 +0530

    Add changes in cors

--- blog/Backend/App.js
+++ blog/Backend/App.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ["http://localhost:5173", 'https://luxury-semifreddo-6566ae.netlify.app'], 
+        origin: 'https://luxury-semifreddo-6566ae.netlify.app', 
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     }
 ));
